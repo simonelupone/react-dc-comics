b16107bf3148d7db2325d24c3d19a7923410f0bd
simonelupone <simone.lupone@gmail.com>
2025-05-06 17:20:08 +0200

    feat: header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ const Header = () => {
     return (
         <header className="bg-white">
             <div className="w-full flex justify-center">
-                <div className="w-7xl mx-12 my-6 flex justify-between items-center">
+                <div className="w-7xl h-32 flex justify-between items-center">
                     <div className="logo">
                         <div className="w-24">
-                            <img src="public/img/dc-logo.png" alt="DC logo"/>
+                            <img src="../src/assets/img/dc-logo.png" alt="DC logo"/>
                         </div>
                     </div>
 
-                    <nav className="h-24 flex items-center">
+                    <nav className="h-full flex items-center">
                         <ul className="h-full flex items-center gap-4 text-slate-800 uppercase font-semibold">
                             <li className="group-hover:text-blue-600"><a href="#">Characters</a></li>
                             <li><a href="#">Comics</a></li>
