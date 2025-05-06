403182aa7ad762d509e499e4b239f9dfb6eba368
simonelupone <simone.lupone@gmail.com>
2025-05-06 17:21:25 +0200

    feat: add components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
-
     return (
-        <Header></Header>
+        <>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
     )
 }
-
 export default App
