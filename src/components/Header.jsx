0436c5a680bf236b9552dccad9b29c177877ab79
simonelupone <simone.lupone@gmail.com>
2025-05-07 17:24:23 +0200

    feat(bonus): loop links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,89 @@
 const Header = () => {
+
+    const links = [
+        {
+            id: 1,
+            href: "#",
+            label: "Characters",
+            current: false,
+        },
+        {
+            id: 2,
+            href: "#",
+            label: "Comics",
+            current: true,
+        },
+        {
+            id: 3,
+            href: "#",
+            label: "Movies",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "TV",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Games",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Collectibles",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Videos",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Fans",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "News",
+            current: false,
+        },
+        {
+            id: 4,
+            href: "#",
+            label: "Shop",
+            current: false,
+        }
+    ]
+
     return (
         <header className="bg-white">
             <div className="w-full flex justify-center">
                 <div className="w-7xl h-32 flex justify-between items-center">
                     <div className="logo">
                         <div className="w-24">
-                            <img src="../src/assets/img/dc-logo.png" alt="DC logo"/>
+                            <img src="../src/assets/img/dc-logo.png" alt="DC logo" />
                         </div>
                     </div>
 
                     <nav className="h-full flex items-center">
                         <ul className="h-full flex items-center gap-4 text-slate-800 uppercase font-semibold">
-                            <li className="group-hover:text-blue-600"><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Collectables</a></li>
-                            <li><a href="#">Video</a></li>
-                            <li><a href="#">Fans</a></li>
-                            <li><a href="#">News</a></li>
-                            <li><a href="#">Shop</a></li>
+
+                            {links.map((link, index) => (
+                                <li key={index} className="group-hover:text-blue-600"><a href={link.href}>{link.label}</a></li>
+                            ))}
                         </ul>
                     </nav>
                 </div>
             </div>
-            
+
             <div className="jumbotron w-full h-140"></div>
         </header>
     )
